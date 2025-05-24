ef3168d65932cb26b5a1c67deff3946eb0dca9cf
helitonns <helitonns@gmail.com>
2025-05-24 14:19:16 -0400

    Configuração do Stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,7 +1,13 @@
-import { colors } from "@heliton-ignite-ui/tokens";
+import { styled } from "./styles";
+
+const Button = styled("button", {
+  fontFamily: "$default",
+  background: "$ignite500",
+  borderRadius: "$sm",
+});
 
 export function App(){
   return(
-    <h1 style={{color: colors.ignite300}}>Hello World!</h1>
+    <Button>Click me</Button>
   );
 }
